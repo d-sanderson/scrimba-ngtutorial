346293e9cc1832b7d46691194b42ddedf4f6beec
dsanderson90 <dsanderson3@cnm.edu>
2018-12-24 10:53:12 -0700

    used ngmodel and got filter functionality working.

--- scrimba-ngtutorial/src/app/customers/customers.module.ts
+++ scrimba-ngtutorial/src/app/customers/customers.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule} from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CustomersListComponent} from "./customers-list/customers-list.component";
+import { FormsModule } from "@angular/forms";
 
 import { SharedModule} from "../shared/shared.module";
 import { CustomersComponent } from './customers.component';
+import { CustomersListComponent} from "./customers-list/customers-list.component";
 import { FilterTextboxComponent } from "./customers-list/filter-textbox.component";
 
 @NgModule({
 	imports: [
-		CommonModule, SharedModule
+		CommonModule, SharedModule, FormsModule
 	],
 	declarations: [
 		CustomersComponent, CustomersListComponent, FilterTextboxComponent
